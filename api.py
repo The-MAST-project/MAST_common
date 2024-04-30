02544616792e0b9d02478661dffefb4b1fb4a1e3
blumzi <theblumz@gmail.com>
2024-04-30 06:24:20 -0800

    ApiClient for calling MAST api endpoints with CanonicalResponses

---
+++ api.py
@@ -0,0 +1,102 @@
+import httpx
+from common.utils import BASE_UNIT_PATH, BASE_SPEC_PATH, init_log
+from enum import Enum, auto
+import re
+import json
+import logging
+import asyncio
+
+
+class ApiDomain(Enum):
+    Unit = auto()
+    Spec = auto()
+
+
+api_ports = {
+    ApiDomain.Unit: 8000,
+    ApiDomain.Spec: 8000,
+}
+
+api_devices = {
+    ApiDomain.Unit: ["mount", "focuser", "camera", "stage", "covers"],
+    ApiDomain.Spec: []
+}
+
+
+class ApiClient:
+
+    def __init__(self, hostname: str, device: str | None = None):
+
+        mast_pattern = re.compile(r"^mast(0[1-9]|1[0-9]|20)$")
+
+        if mast_pattern.match(hostname):
+            self.domain = ApiDomain.Unit
+        elif hostname == "spec":
+            self.domain = ApiDomain.Spec
+        else:
+            raise Exception(f"Bad {hostname=}.  Allowed hostnames are [mast01..mast20] or spec")
+
+        domain_base = BASE_UNIT_PATH if self.domain == ApiDomain.Unit else BASE_SPEC_PATH
+
+        self.base_url = f"http://{hostname}:{api_ports[self.domain]}/{domain_base}"
+        if device:
+            self.base_url += f"/{device}"
+        self.logger = logging.getLogger(f"api-client")
+        init_log(self.logger)
+
+    async def get(self, method: str, params: dict | None = None):
+        try:
+            async with httpx.AsyncClient() as client:
+                response = await client.get(url=f"{self.base_url}/{method}", params=params)
+                response.raise_for_status()
+                canonical_response = response.json()
+                if 'exception' in canonical_response:
+                    self.logger.error(f"Remote exception: {canonical_response['exception']}")
+                elif 'errors' in canonical_response:
+                    for err in canonical_response['errors']:
+                        self.logger.error(f"Remote error: {err}")
+                else:
+                    return json.loads(canonical_response['value'])
+
+        except httpx.HTTPStatusError as e:
+            self.logger.error(f"HTTP error (url={e.request.url}): {e.response.status_code} - {e.response.text}")
+        except httpx.RequestError as e:
+            self.logger.error(f"Request error (url={e.request.url}): {e}")
+        except Exception as e:
+            self.logger.error(f"An error occurred: {e}")
+
+    async def put(self, method: str, params: dict | None = None):
+        try:
+            async with httpx.AsyncClient() as client:
+                response = await client.put(url=f"{self.base_url}/{method}", params=params)
+                response.raise_for_status()
+                canonical_response = response.json()
+                if 'exception' in canonical_response:
+                    self.logger.error(f"Remote exception: {canonical_response['exception']}")
+                elif 'errors' in canonical_response:
+                    for err in canonical_response['errors']:
+                        self.logger.error(f"Remote error: {err}")
+                else:
+                    return json.loads(canonical_response['value'])
+
+        except httpx.HTTPStatusError as e:
+            self.logger.error(f"HTTP error (url={e.request.url}): {e.response.status_code} - {e.response.text}")
+        except httpx.RequestError as e:
+            self.logger.error(f"Request error (url={e.request.url}): {e}")
+        except Exception as e:
+            self.logger.error(f"An error occurred: {e}")
+
+
+async def main():
+    unit = ApiClient(hostname='mast01')
+    response = await unit.get('status')
+    if response:
+        print(response)
+
+    focuser = ApiClient(hostname='mast01', device='focuser')
+    response = await focuser.get('status')
+    if response:
+        print(response)
+
+if __name__ == '__main__':
+    asyncio.run(main())
