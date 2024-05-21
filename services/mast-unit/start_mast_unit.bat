80ae3bfc5339cbf8b612fbb48e2c3d210546c948
blumzi <theblumz@gmail.com>
2024-05-21 01:07:47 -0800

    batch script for running the unit service

---
+++ services/mast-unit/start_mast_unit.bat
@@ -0,0 +1,9 @@
+@echo off
+
+set top=C:\Users\mast\PycharmProjects\MAST_unit
+set venv=%top%\venv
+set MAST_PROJECT=unit
+set PYTHONUNBUFFERED=1
+
+CALL %venv%\Scripts\activate.bat
+%venv%\Scripts\python.exe %top%\src\app.py > NUL 2>&1
