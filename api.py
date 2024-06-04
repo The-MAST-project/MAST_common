33e134166be24ef9e0d279c29487580138f10334
Arie Blumenzweig <theblumz@gmail.com>
2024-06-04 13:09:00 +0100

    merged

--- api.py
+++ api.py
@@ -62,7 +62,7 @@ class ApiClient:
 
     def __init__(self, hostname: str, device: str | None = None, timeout: float = TIMEOUT):
 
-        mast_pattern = re.compile(r"^mast(0[1-9]|1[0-9]|20)$")
+        mast_pattern = re.compile(r"^mast(0[1-9]|1[0-9]|20|w)$")
 
         if mast_pattern.match(hostname):
             self.domain = ApiDomain.Unit
@@ -72,6 +72,9 @@ class ApiClient:
             raise ValueError(f"bad {hostname=}.  Allowed hostnames are [mast01..mast20] or spec")
 
         domain_base = BASE_UNIT_PATH if self.domain == ApiDomain.Unit else BASE_SPEC_PATH
+
+        if not hostname.endswith('.weizmann.ac.il'):
+            hostname += '.weizmann.ac.il'
 
         self.base_url = f"http://{hostname}:{api_ports[self.domain]}{domain_base}"
         if device:
@@ -86,22 +89,32 @@ class ApiClient:
 
         response: dict = self.get('status')
         if response:
-            self.detected = response['detected']
-            self.operational = response['operational']
+            self.detected = response['detected'] if 'detected' in response else False
+            self.operational = response['operational'] if 'operational' in response else False
 
     def get(self, method: str, params: dict | None = None) -> dict | None:
-        with httpx.Client() as client:
+        url = f"{self.base_url}/{method}"
+        with httpx.Client(trust_env=False) as client:
             try:
-                response = client.get(url=f"{self.base_url}/{method}", params=params, timeout=self.timeout)
-            except:
-                return None
+                response = client.get(url=url, params=params, timeout=self.timeout)
+            except httpx.TimeoutException:
+                logger.error(f"timeout after {self.timeout} seconds, {url=}")
+                return {'error': 'timeout'}
+            except Exception as e:
+                logger.error(f"exception: {e}")
+                return {'error': f"{e}"}
         return self.common_get_put(response)
 
     def put(self, method: str, params: dict | None = None) -> dict:
-        with httpx.AsyncClient() as client:
+        url = f"{self.base_url}/{method}"
+        with httpx.Client(trust_env=False) as client:
             try:
                 response = client.put(url=f"{self.base_url}/{method}", params=params, timeout=self.timeout)
+            except httpx.TimeoutException:
+                logger.error(f"timeout after {self.timeout} seconds, {url=}")
+                return {'error': 'timeout'}
             except Exception as e:
+                logger.error(f"exception: {e}")
                 return {'error': f"{e}"}
         return self.common_get_put(response)
 
@@ -111,17 +124,15 @@ class ApiClient:
 
         try:
             response.raise_for_status()
-            canonical_response = response.json()
-            if hasattr(canonical_response, 'exception'):
+            canonical_response = response.json()['response']
+            if 'exception' in canonical_response:
                 # logger.error(f"Remote exception:")
-                # for line in canonical_response.exception:
+                # for line in canonical_response['exception']:
                 #     logger.error(f"  [E] {line.removesuffix('\n')}")
-                logger.error(f"Remote exception: {canonical_response.exception}")
-            elif hasattr(canonical_response, 'errors'):
-                for err in canonical_response.errors:
+                logger.error(f"Remote exception: {canonical_response['exception']}")
+            elif 'errors' in canonical_response:
+                for err in canonical_response['errors']:
                     logger.error(f"Remote error: {err}")
-            elif hasattr(canonical_response, 'value'):
-                return canonical_response.value
             else:
                 raise Exception(f"missing 'value' attribute in canonical_response '{canonical_response}'")
 
