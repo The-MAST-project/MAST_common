c3100eb0e3d717c28f77141c2c7dca14adb945c0
blumzi <theblumz@gmail.com>
2024-12-11 10:38:09 +0200

    added initiator to SolvingResult

--- solving.py
+++ solving.py
@@ -20,17 +20,29 @@ class SolvingResult:
     succeeded: bool | None = None
     errors: Optional[List[str]] = None
     solution: SolvingSolution | None = None
-    result = None
+    solver_result = None
     elapsed_seconds: float | None = None
 
+    def __init__(self, succeeded: Optional[bool] = None,
+                 errors: Optional[List[str]] = None,
+                 solution: Optional[SolvingSolution] = None,
+                 solver_result = None):
+        self.succeeded = succeeded
+        self.errors = errors
+        self.solution = solution
+        self.solver_result = solver_result
+
     def to_dict(self):
-        return {
+        ret = {
             'succeeded': self.succeeded,
             'errors': self.errors,
             'solution': self.solution.to_dict(),
-            # 'result': self.result.to_dict(),
             'elapsed_seconds': self.elapsed_seconds,
         }
+        if self.solver_result and hasattr(self.solver_result, 'to_dict'):
+            ret['solving_result'] = self.solver_result.to_dict()
+
+        return ret
 
 
 class SolvingTolerance:
