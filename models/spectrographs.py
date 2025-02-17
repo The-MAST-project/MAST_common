73cf300026a8a5d4e29e55df827100eb642246d9
Arie Blumenzweig <theblumz@gmail.com>
2025-02-17 15:04:38 +0200

    pydantic v2 discriminator

--- models/spectrographs.py
+++ models/spectrographs.py
@@ -1,5 +1,5 @@
-from pydantic import BaseModel, Field, field_validator, ValidationError, model_validator
-from typing import Optional, Literal, Union
+from pydantic import BaseModel, Field
+from typing import Optional, Union
 from common.models.deepspec import DeepspecModel
 from common.models.highspec import HighspecModel
 from common.models.calibration import CalibrationModel
@@ -9,9 +9,5 @@ class SpectrographModel(BaseModel):
     exposure: float
     number_of_exposures: Optional[int] = 1
     calibration: Optional[CalibrationModel]
-    spec: Union[HighspecModel, DeepspecModel]
+    spec: Union[HighspecModel, DeepspecModel] = Field(discriminator='instrument')
 
-    class Config:
-        # Set the discriminating field
-        smart_union = True
-        discriminator = 'instrument'
