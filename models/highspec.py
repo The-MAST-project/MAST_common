1bbf18af5b1353e3eca669412bfbd17109be799c
Arie Blumenzweig <theblumz@gmail.com>
2025-02-18 18:32:31 +0200

    disperser

--- models/highspec.py
+++ models/highspec.py
@@ -2,8 +2,9 @@ from pydantic import BaseModel
 from typing import Literal, Optional
 
 from common.models.newton import NewtonCameraSettingsModel
+from common.spec import Disperser
 
 class HighspecModel(BaseModel):
     instrument: Literal['highspec']
-    disperser: Literal
+    disperser: Disperser
     camera: Optional[NewtonCameraSettingsModel]
