d607e8254cf45f3bed72a03899acf1a0c220b5d6
Arie Blumenzweig <theblumz@gmail.com>
2025-02-18 18:08:03 +0200

    reverted to Any (for the meantime)

--- models/spectrographs.py
+++ models/spectrographs.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, Field
-from typing import Optional, Union, Literal
+from typing import Optional, Union, Literal, Any
 from common.models.deepspec import DeepspecModel
 from common.models.highspec import HighspecModel
 from common.models.calibration import CalibrationModel
@@ -10,5 +10,5 @@ class SpectrographModel(BaseModel):
     exposure: float
     number_of_exposures: Optional[int] = 1
     calibration: Optional[CalibrationModel]
-    spec: Union[HighspecModel, DeepspecModel] = Field(discriminator='instrument')
+    spec: Any
 
