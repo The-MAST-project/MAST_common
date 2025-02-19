e12c47aff00408bc9d4b01d31487ca24beba1b5b
Arie Blumenzweig <theblumz@gmail.com>
2025-02-19 10:07:57 +0200

    another attempt at making the spec field a union of models

--- models/spectrographs.py
+++ models/spectrographs.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel, Field
-from typing import Optional, Union, Literal, Any
+from typing import Optional, Union, Literal
+
+# from common.models.assignments import HighSpecAssignment
 from common.models.deepspec import DeepspecModel
 from common.models.highspec import HighspecModel
 from common.models.calibration import CalibrationModel
@@ -10,5 +12,5 @@ class SpectrographModel(BaseModel):
     exposure: float
     number_of_exposures: Optional[int] = 1
     calibration: Optional[CalibrationModel]
-    spec: Any
+    spec: Union[HighspecModel, DeepspecModel] = Field(discriminator='instrument')
 
