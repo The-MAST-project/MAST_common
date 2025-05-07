fcab1c4e9b7c797a41a2e26466b598da6cdb629b
Arie Blumenzweig <theblumz@gmail.com>
2025-05-07 16:46:56 +0300

    black

--- solving.py
+++ solving.py
@@ -1,6 +1,7 @@
-from typing import Optional, List, Literal
-from astropy.coordinates import Angle
 from enum import IntFlag, auto
+from typing import List, Literal, Optional
+
+from astropy.coordinates import Angle
 
 
 class SolverId(IntFlag):
@@ -10,7 +11,7 @@ class SolverId(IntFlag):
     # Astap = auto()
 
 
-SolverIdNames = Literal['PlaneWaveCli', 'PlaneWaveShm', 'AstrometryDotNet']
+SolverIdNames = Literal["PlaneWaveCli", "PlaneWaveShm", "AstrometryDotNet"]
 
 
 class SolvingSolution:
@@ -25,14 +26,14 @@ class SolvingSolution:
 
     def to_dict(self):
         return {
-            'ra_rads': self.ra_rads,
-            'dec_rads': self.dec_rads,
-            'ra_hours': self.ra_hours,
-            'dec_degs': self.dec_degs,
-            'matched_stars': self.matched_stars,
-            'catalog_stars': self.catalog_stars,
-            'rotation_angle_degs': self.rotation_angle_degs,
-            'pixel_scale': self.pixel_scale,
+            "ra_rads": self.ra_rads,
+            "dec_rads": self.dec_rads,
+            "ra_hours": self.ra_hours,
+            "dec_degs": self.dec_degs,
+            "matched_stars": self.matched_stars,
+            "catalog_stars": self.catalog_stars,
+            "rotation_angle_degs": self.rotation_angle_degs,
+            "pixel_scale": self.pixel_scale,
         }
 
 
@@ -43,10 +44,13 @@ class SolvingResult:
     solver_result = None
     elapsed_seconds: float | None = None
 
-    def __init__(self, succeeded: Optional[bool] = None,
-                 errors: Optional[List[str]] = None,
-                 solution: Optional[SolvingSolution] = None,
-                 solver_result=None):
+    def __init__(
+        self,
+        succeeded: Optional[bool] = None,
+        errors: Optional[List[str]] = None,
+        solution: Optional[SolvingSolution] = None,
+        solver_result=None,
+    ):
         self.succeeded = succeeded
         self.errors = errors
         self.solution = solution
@@ -54,13 +58,13 @@ class SolvingResult:
 
     def to_dict(self):
         ret = {
-            'succeeded': self.succeeded,
-            'errors': self.errors,
-            'solution': self.solution.to_dict(),
-            'elapsed_seconds': self.elapsed_seconds,
+            "succeeded": self.succeeded,
+            "errors": self.errors,
+            "solution": self.solution.to_dict(),
+            "elapsed_seconds": self.elapsed_seconds,
         }
-        if self.solver_result and hasattr(self.solver_result, 'to_dict'):
-            ret['solving_result'] = self.solver_result.to_dict()
+        if self.solver_result and hasattr(self.solver_result, "to_dict"):
+            ret["solving_result"] = self.solver_result.to_dict()
 
         return ret
 
