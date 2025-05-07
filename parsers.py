aa2f0b2e6d75a8de74b238210c41cf3caa6a3e39
Arie Blumenzweig <theblumz@gmail.com>
2025-05-07 16:50:00 +0300

    black

--- parsers.py
+++ parsers.py
@@ -9,7 +9,7 @@ from common.utils import function_name
 from common.mast_logging import init_log
 import re
 
-logger = logging.Logger('parsers')
+logger = logging.Logger("parsers")
 init_log(logger)
 
 
@@ -45,26 +45,30 @@ def parse_units(specifiers: List[str] | str) -> List[str]:
             building = None
             building_name: str | None = None
 
-            match = re.match(r'^(?:(?P<site>\w+):)?(?:(?P<building>\w+):)?(?P<units>[,a-zA-Z0-9_-]+)$', spec)
+            match = re.match(
+                r"^(?:(?P<site>\w+):)?(?:(?P<building>\w+):)?(?P<units>[,a-zA-Z0-9_-]+)$",
+                spec,
+            )
             if match:
                 # <site>:<building>:<units>
                 site_name = match.group(1)
                 building_name = match.group(2)
                 units_spec = match.group(3)
             else:
-                match = re.match(r'^(?:(?P<site>\w+):{1,2})?(?P<units>\w+)$', spec)
+                match = re.match(r"^(?:(?P<site>\w+):{1,2})?(?P<units>\w+)$", spec)
                 if match:
                     # <site>:<units>
-                    site_name = match.group('site')
-                    units_spec = match.group('units')
+                    site_name = match.group("site")
+                    units_spec = match.group("units")
                 else:
                     logger.error(f"{op}: Invalid units spec: {specifier}")
                     continue
 
-
             if site_name:
                 if site_name not in [s.name for s in sites]:
-                    logger.error(f"{op}: Invalid site: '{site_name}', defined sites: {[s.name for s in sites]}")
+                    logger.error(
+                        f"{op}: Invalid site: '{site_name}', defined sites: {[s.name for s in sites]}"
+                    )
                     continue
                 else:
                     site = [s for s in sites if s.name == site_name][0]
@@ -80,7 +84,9 @@ def parse_units(specifiers: List[str] | str) -> List[str]:
 
                 if not building:
                     # a building was specified but it's not valid
-                    logger.error(f"{op}: Invalid building: '{building_name}' at site '{site.name}'")
+                    logger.error(
+                        f"{op}: Invalid building: '{building_name}' at site '{site.name}'"
+                    )
                     continue
 
             for unit in parse_unit_ids(units_spec):
@@ -88,27 +94,44 @@ def parse_units(specifiers: List[str] | str) -> List[str]:
                     if unit not in building.units:
                         logger.error(f"{op}: {unit=} not in {building.units=}")
                         continue
-                    units_numbering_base = sum([len(b.units) for b in site.buildings[0:site.buildings.index(building)]])
+                    units_numbering_base = sum(
+                        [
+                            len(b.units)
+                            for b in site.buildings[0 : site.buildings.index(building)]
+                        ]
+                    )
                     unit_id = str(int(unit) + units_numbering_base)
                     if unit_id not in site.valid_ids:
-                        logger.error(f"{op}: {unit_id=} not valid at '{site.name}' ({site.valid_ids=}), skipped.")
+                        logger.error(
+                            f"{op}: {unit_id=} not valid at '{site.name}' ({site.valid_ids=}), skipped."
+                        )
                     elif unit_id not in site.deployed_units:
-                        logger.error(f"{op}: {unit_id=} not deployed at '{site.name}' ({site.deployed_units=}), skipped.")
+                        logger.error(
+                            f"{op}: {unit_id=} not deployed at '{site.name}' ({site.deployed_units=}), skipped."
+                        )
                     elif unit_id in site.units_in_maintenance:
-                        logger.error(f"{op}: {unit_id=} in maintenance at '{site.name}' ({site.units_in_maintenance=}), skipped.")
+                        logger.error(
+                            f"{op}: {unit_id=} in maintenance at '{site.name}' ({site.units_in_maintenance=}), skipped."
+                        )
                     else:
                         ret.append(f"{site_name}:{unit_id}")
 
                 elif unit in site.valid_ids:
                     unit_id = unit
                     if unit_id not in site.deployed_units:
-                        logger.error(f"{op}: {unit_id=} not deployed at '{site.name}' ({site.deployed_units=}), skipped.")
+                        logger.error(
+                            f"{op}: {unit_id=} not deployed at '{site.name}' ({site.deployed_units=}), skipped."
+                        )
                     elif unit_id in site.units_in_maintenance:
-                        logger.error(f"{op}: {unit_id=} in maintenance at '{site.name}' ({site.units_in_maintenance=}), skipped.")
+                        logger.error(
+                            f"{op}: {unit_id=} in maintenance at '{site.name}' ({site.units_in_maintenance=}), skipped."
+                        )
                     else:
                         ret.append(f"{site_name}:{unit_id}")
                 else:
-                    logger.error(f"{op}: Invalid unit: '{unit}' at '{site.name=}', known units: {site.valid_ids}")
+                    logger.error(
+                        f"{op}: Invalid unit: '{unit}' at '{site.name=}', known units: {site.valid_ids}"
+                    )
 
     return ret
 
@@ -122,11 +145,11 @@ def parse_unit_ids(units_spec: str) -> List[str]:
     """
     ret = []
 
-    for spec in units_spec.split(','):
-        if '-' in spec:
-            word = spec.split('-')
+    for spec in units_spec.split(","):
+        if "-" in spec:
+            word = spec.split("-")
             if word[0].isdigit() and word[1].isdigit():
-                for i in range(int(word[0]), int(word[1])+1):
+                for i in range(int(word[0]), int(word[1]) + 1):
                     ret.append(str(i))
         else:
             ret.append(spec)
@@ -154,7 +177,7 @@ def sexagesimal_degrees_to_decimal(value: str | float) -> float:
         raise
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # units = parse_units(['w', 'ns:10-12', 'ns:1,3,5', 'ns:south:3-5'])
     # print(units)
-    print(sexagesimal_degrees_to_decimal('2:53:32.8 '))
+    print(sexagesimal_degrees_to_decimal("2:53:32.8 "))
