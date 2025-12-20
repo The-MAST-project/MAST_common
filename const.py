4c74f93e0d82ba69b006e6dc4dc9358b4dde820b
Arie Blumenzweig <theblumz@gmail.com>
2025-12-20 11:37:35 +0200

    controller related stuff

--- const.py
+++ const.py
@@ -2,15 +2,16 @@ from typing import Literal
 
 
 class Const:
-
     WEIZMANN_DOMAIN: str = "weizmann.ac.il"
 
     BASE_SPEC_PATH: str = "/mast/api/v1/spec"
     BASE_UNIT_PATH: str = "/mast/api/v1/unit"
     BASE_CONTROL_PATH: str = "/mast/api/v1/control"
-
+    BASE_DATA_PATH: str = BASE_CONTROL_PATH + "/data"
 
     PLATE_SOLVING_SHM_NAME: str = "PlateSolving_Image"
 
-    SolvingPhase = Literal["sky", "spec"]   # acquisition phases that use plate solving
-    CorrectionPhase = SolvingPhase          # acquisition phases that use corrections
+    SolvingPhase = Literal["sky", "spec"]  # acquisition phases that use plate solving
+    CorrectionPhase = SolvingPhase  # acquisition phases that use corrections
+
+    PlanFileNamePattern = "PLAN_*.toml"
