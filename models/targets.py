c36be144cab77a8580a366c04bdcb159c38f33fb
Arie Blumenzweig <theblumz@gmail.com>
2026-03-12 09:56:57 +0200

    ra_hours and dec_degrees to be more explicit

--- models/targets.py
+++ models/targets.py
@@ -3,10 +3,10 @@ from pydantic import BaseModel, Field, field_validator
 
 
 class Target(BaseModel):
-    ra: str | float = Field(description="RightAscension [sexagesimal or decimal]")
-    dec: str | float = Field(description="Declination [sexagesimal or decimal]")
+    ra_hours: str | float = Field(description="RightAscension [sexagesimal or decimal]")
+    dec_degrees: str | float = Field(description="Declination [sexagesimal or decimal]")
 
-    @field_validator("ra")
+    @field_validator("ra_hours")
     @classmethod
     def validate_ra(cls, value):
         """
@@ -17,7 +17,7 @@ class Target(BaseModel):
         ra = astropy.coordinates.Longitude(value, unit="hour").value
         return float(ra)  # converts np.float64 to float
 
-    @field_validator("dec")
+    @field_validator("dec_degrees")
     @classmethod
     def validate_dec(cls, value):
         """
@@ -27,3 +27,6 @@ class Target(BaseModel):
         """
         dec = astropy.coordinates.Latitude(value, unit="deg").value
         return float(dec)  # converts np.float64 to float
+
+    def __repr__(self) -> str:
+        return f"Target(ra_hours={self.ra_hours}, dec_degrees={self.dec_degrees})"
