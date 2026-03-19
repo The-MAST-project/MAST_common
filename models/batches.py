accbf6531d732fdaa49900b96a33133829f02d0a
Arie Blumenzweig <theblumz@gmail.com>
2026-03-19 10:16:30 +0200

    one more attempt at normalizing the list of plans into a Batch

--- models/batches.py
+++ models/batches.py
@@ -3,15 +3,14 @@ from ulid import ULID
 
 from common.models.calibration import CalibrationSettings
 from common.models.plans import Plan
+from common.models.spectrographs import SpectrographModel
 
 
 class Batch(BaseModel):
     ulid: ULID | None = None
     immediate: bool = False  # for immediate execution or merely forecasted
     plans: list[Plan]
-    calibration: CalibrationSettings | None = None
-    exposure_duration: float | None = None
-    number_of_exposures: int | None = None
+    spec_assignment: SpectrographModel | None = None
     predicted_duration: float | None = None
 
     def model_post_init(self) -> None:
@@ -19,29 +18,79 @@ class Batch(BaseModel):
         Make a batch out of the given plans.
         """
 
+        assert all(plan.spec_assignment is not None for plan in self.plans), (
+            "All plans must have a spectrograph assignment"
+        )
+        assert all(
+            plan.spec_assignment.instrument == self.plans[0].spec_assignment.instrument
+            for plan in self.plans
+        ), "All plans must have the same spectrograph instrument"
+
+        # if any of the plans require autofocus, we need to add a predicted autofocus duration to the batch
         predicted_autofocus_duration = (
             180 if any(plan.autofocus for plan in self.plans) else 0
         )
 
         self.ulid = self.ulid or ULID()
 
-        self.number_of_exposures = max(
-            plan.spec.number_of_exposures
-            for plan in self.plans
-            if plan.spec.number_of_exposures is not None
-        )
+        #
+        # We need to merge the spectrograph assignments of the plans into a single assignment for the batch.
+        #
 
         #
         # TBD: How to handle the case where different plans have different exposure durations
         #   Some plans may become over-exposed.
         #
-        self.exposure_duration = max(
-            plan.spec.exposure_duration
-            for plan in self.plans
-            if plan.spec.exposure_duration is not None
+        exposure_duration = max(
+            [plan.spec_assignment.exposure_duration for plan in self.plans]
         )
-        self.calibration = CalibrationSettings(lamp_on=False, filter=None)
-        max_density = 0
+
+        number_of_exposures = (
+            max(
+                [
+                    plan.spec_assignment.number_of_exposures
+                    for plan in self.plans
+                    if plan.spec_assignment.number_of_exposures is not None
+                ]
+            )
+            or 1
+        )
+
+        requested_calibration = CalibrationSettings(lamp_on=False, filter=None)
+        requested_calibration.lamp_on = any(
+            plan.spec_assignment.calibration.lamp_on
+            for plan in self.plans
+            if plan.spec_assignment.calibration is not None
+        )
+
+        if requested_calibration.lamp_on:
+            requested_filters = [
+                plan.spec_assignment.calibration.filter
+                for plan in self.plans
+                if plan.spec_assignment.calibration is not None
+                and plan.spec_assignment.calibration.lamp_on
+                and plan.spec_assignment.calibration.filter is not None
+            ]
+            filter_densities = [
+                int(f.replace("ND", ""))
+                for f in requested_filters
+                if f.startswith("ND")
+            ]
+            requested_calibration.filter = (
+                str(max(filter_densities)) if filter_densities else None
+            )
+            if requested_calibration.filter is not None:
+                requested_calibration.filter = f"ND{requested_calibration.filter}"
+
+        self.spec_assignment = SpectrographModel(
+            instrument=self.plans[0].spec_assignment.instrument,
+            exposure_duration=exposure_duration,
+            number_of_exposures=number_of_exposures,
+            calibration=requested_calibration,
+        )
+
+        # TBD: How to handle the case where different plans have different spec settings?
+
         max_timeout_to_guiding = 0
         for plan in self.plans:
             if (
@@ -50,23 +99,8 @@ class Batch(BaseModel):
             ):
                 max_timeout_to_guiding = plan.timeout_to_guiding
 
-            calibration = plan.spec.calibration
-            if calibration is None:
-                continue
-
-            if calibration.lamp_on:
-                self.calibration.lamp_on = True
-                if calibration.filter is not None:
-                    density = (
-                        int(calibration.filter.replace("ND", ""))
-                        if calibration.filter.startswith("ND")
-                        else 0
-                    )
-                    if density > max_density:
-                        max_density = density
-                        self.calibration.filter = calibration.filter
         self.predicted_duration = (
             predicted_autofocus_duration
             + max_timeout_to_guiding
-            + self.exposure_duration * self.number_of_exposures
+            + exposure_duration * number_of_exposures
         )
