1e0c40a263da81111ae260557c177be1e2a6d331
Arie Blumenzweig <theblumz@gmail.com>
2026-04-14 10:28:09 +0300

    deepspec sensor temperature

--- models/statuses.py
+++ models/statuses.py
@@ -223,8 +223,8 @@ class GreateyesStatus(ComponentStatus):
     addr: int | None = None
     min_temp: float | None = None
     max_temp: float | None = None
-    temperature_adjustment_target: float | None = None
-    front_temperature: float | None = None
+    sensor_temperature_target: float | None = None
+    sensor_temperature: float | None = None
     back_temperature: float | None = None
     errors: list[str] | None = None
     latest_exposure: Any = None
