8ffd6da69da7a7b3e8ff9d95de1f811ba4502571
Arie Blumenzweig <theblumz@gmail.com>
2026-04-26 17:45:05 +0300

    added required_units to ScrapingResults

--- models/plan_scraping.py
+++ models/plan_scraping.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, Field
 from common.utils import time_stamp
 
+
 #
 # These models are used to store the results of scraping MAST plans.
 #
@@ -8,9 +9,11 @@ class OwnersModel(BaseModel):
     name: str | None = None
     uuid: str | None = None
 
+
 class ScrapingResults(BaseModel):
     owners: list[OwnersModel] | None = None
     classifications: list[str] | None = None
     known_classifications: list[str] | None = None
     allocated_units: list[str] | None = None
+    required_units: int | None = None
     scraped_at: str = Field(default_factory=time_stamp)
